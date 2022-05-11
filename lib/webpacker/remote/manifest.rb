3eb6a3663368f027d4e050ca72ad752257aa7573
Vlad Bokov <vladboko@amazon.co.uk>
2022-05-11 15:57:27 +0100

    Support webpack-asset-manifest-5

--- lib/webpacker/remote/manifest.rb
+++ lib/webpacker/remote/manifest.rb
@@ -8,9 +8,20 @@ class Webpacker::Remote::Manifest < Webpacker::Manifest
   end
 
   def lookup_pack_with_chunks(name, pack_type = {})
-    return unless (paths = super)
+    # `super` method copy
+    manifest_pack_type = manifest_type(pack_type[:type])
+    manifest_pack_name = manifest_name(name, manifest_pack_type)
+    assets = find('entrypoints')[manifest_pack_name]
+    # patched super behavior:
+    # - keys in webpack-assets-manifest-3: entrypoints.main.{js/css}
+    # - keys in webpack-assets-manifest-5: entrypoints.main.assets.{js/css}
+    assets = assets['assets'] if assets.key?('assets')
+    paths = assets[manifest_pack_type]
+    # end of `super` method copy
 
     paths.map { |p| File.join(config.root_path.to_s, p) }
+  rescue NoMethodError
+    nil
   end
 
   def lookup(name, pack_type = {})
